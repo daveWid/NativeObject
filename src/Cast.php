de131513e54131374781f2e855f9eb8ea717f8f2
Dave Widmer <dave@davewidmer.net>
2014-01-17 10:45:03 -0500

    Adding in some DocBlocks

--- src/Cast.php
+++ src/Cast.php
@@ -12,22 +12,39 @@ class Cast
 	const TIME_PATTERN = 'H:i:s';
 	const TIMESTAMP_PATTERN = 'U';
 
+	/**
+	 * @param  mixed $source
+	 * @return false
+	 */
 	public static function toBoolean($source)
 	{
 		$false = array('', 'false', false, null, 0, '0');
 		return  ! \in_array($source, $false);
 	}
 
+	/**
+	 * @param  string $source
+	 * @return int
+	 */
 	public static function toInteger($source)
 	{
 		return (int) $source;
 	}
 
+	/**
+	 * @param  string $source
+	 * @return float
+	 */
 	public static function toFloat($source)
 	{
 		return (float) $source;
 	}
 
+	/**
+	 * @param  string $source
+	 * @param  string $pattern  The pattern to convert to a DateTime object
+	 * @return \DateTime
+	 */
 	public static function toDateTime($source, $pattern = null)
 	{
 		if ($pattern === null)
@@ -38,21 +55,41 @@ class Cast
 		return \DateTime::createFromFormat($pattern, $source);
 	}
 
+	/**
+	 * @param  string $source
+	 * @return \DateTime
+	 */
 	public static function toDate($source)
 	{
 		return self::toDateTime($source, self::DATE_PATTERN);
 	}
 
+	/**
+	 * @param  string $source
+	 * @return \DateTime
+	 */
 	public static function toTime($source)
 	{
 		return self::toDateTime($source, self::TIME_PATTERN);
 	}
 
+	/**
+	 * @param  mixed $source
+	 * @return \DateTime
+	 */
 	public static function toTimestamp($source)
 	{
 		return self::toDateTime($source, self::TIMESTAMP_PATTERN);
 	}
 
+	/**
+	 * @param  string    $source
+	 * @param  callable  $callback  An value that can be used in \call_user_func
+	 *
+	 * @uses \call_user_func
+	 *
+	 * @return \DateTime
+	 */
 	public static function toCallback($source, $callback)
 	{
 		return \call_user_func($callback, $source);
